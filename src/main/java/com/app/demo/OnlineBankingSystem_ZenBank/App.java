ce68c436ca4231b5b29b98d94b8ad896e42d12f3
Chetan <kolakantichethan@gmail.com>
2024-11-13 11:20:49 +0530

    Sample Maven Java

---
+++ src/main/java/com/app/demo/OnlineBankingSystem_ZenBank/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.OnlineBankingSystem_ZenBank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
